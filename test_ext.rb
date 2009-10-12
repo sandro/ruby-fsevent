9cc61857853a71bc385781a4831586747969da49
Sandro Turriate <sandro.turriate@gmail.com>
2009-10-11 22:10:58 -0400

    Update ruby test

--- test_ext.rb
+++ test_ext.rb
@@ -6,9 +6,20 @@ trap("INT") do
 end
 
 class LibWatch < Watch
+  def something
+    puts "something called"
+  end
   def directory_change(file)
+    puts "ruby callback"
     puts file
   end
 end
 
 l = LibWatch.new('.')
+
+at_exit do
+  puts "exiting"
+  p l
+end
+
+l.run
