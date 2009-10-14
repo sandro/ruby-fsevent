5389ec74583b85c40794d090a32bfec23523ca56
Sandro Turriate <sandro.turriate@gmail.com>
2009-10-13 23:06:46 -0400

    Cleanup

--- test_ext.rb
+++ test_ext.rb
@@ -1,18 +1,9 @@
 require 'watch'
 
-trap("INT") do
-  puts 'Signal trapped'
-  exit 0
-end
-
 class LibWatch < Watch
   def initialize(dir)
     super(dir)
     @latency = 0.5
-  end
-
-  def something
-    puts "something called"
   end
 
   def directory_change(directory)
