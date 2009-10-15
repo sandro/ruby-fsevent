7e469ddfc1e956ca858ee17d430ae65603baf3d8
Sandro Turriate <sandro.turriate@gmail.com>
2009-10-15 00:28:48 -0400

    Use new interface

--- test_ext.rb
+++ test_ext.rb
@@ -1,15 +1,16 @@
 require 'watch'
 
 class LibWatch < Watch
-  def initialize(dir)
-    super(dir)
-    @latency = 0.5
-  end
+  attr_accessor :latency
+  attr_reader :watch_pid
 
   def directory_change(directory)
     puts "Ruby callback: #{directory.inspect}"
   end
 end
 
-l = LibWatch.new('.')
+l = LibWatch.new
+l.latency = 0.2
+# l.watch_directories "."
+l.watch_directories %w(. /tmp)
 l.run
